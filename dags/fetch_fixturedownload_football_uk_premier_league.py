dcd07c377ac56d2aa71ce74bd79399b05a8d8474
Asfand Qazi <ayqazi@gmail.com>
2024-10-07 00:42:15 +0100

    Remove comments

--- dags/fetch_fixturedownload_football_uk_premier_league.py
+++ dags/fetch_fixturedownload_football_uk_premier_league.py
@@ -9,23 +9,6 @@ from airflow.providers.amazon.aws.transfers.http_to_s3 import HttpToS3Operator
      description="Download UK Premier League football fixtures data",
      catchup=False)
 def fetch_fixturedownload_football_uk_premier_league():
-    # @task()
-    # def fetch() -> List[Dict]:
-    #     year = datetime.now(UTC).year
-    #     import urllib.request
-    #     url = f"https://fixturedownload.com/feed/json/epl-{year}"
-    #     with urllib.request.urlopen(url) as file:
-    #         logging.info(f"Fetching {url}")
-    #         return json.load(file)
-
-    # create_object = S3CreateObjectOperator(
-    #     task_id="create_object",
-    #     s3_bucket="com-cloudership-prod-eu-north-1-airflow-testing",
-    #     s3_key="",
-    #     data=DATA,
-    #     replace=True,
-    # )
-
     HttpToS3Operator(
         task_id="fetch",
         http_conn_id="http_fixturedownload_com",
