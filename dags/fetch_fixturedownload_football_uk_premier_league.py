3a3e0efdc2d868215d8b36c6ce1a960230cb8f46
Asfand Qazi <ayqazi@gmail.com>
2024-10-08 18:33:09 +0100

    Do enough to get football fixture downloader working

--- dags/fetch_fixturedownload_football_uk_premier_league.py
+++ dags/fetch_fixturedownload_football_uk_premier_league.py
@@ -1,22 +1,35 @@
-from datetime import datetime
+import logging
+from datetime import datetime, UTC
 
-from airflow.decorators import dag
-from airflow.providers.amazon.aws.transfers.http_to_s3 import HttpToS3Operator
+import pendulum
+import requests
+from airflow.decorators import dag, task
+from airflow.io.path import ObjectStoragePath
+
+YEAR = 2024
+BUCKET_ROOT = ObjectStoragePath("s3://com-cloudership-prod-eu-north-1-airflow-testing/", conn_id="aws_default")
 
 
 @dag(schedule="@daily",
-     start_date=datetime(2024, 1, 1),
+     start_date=datetime(2024, 1, 1, tzinfo=UTC),
      description="Download UK Premier League football fixtures data",
      catchup=False)
 def fetch_fixturedownload_football_uk_premier_league():
-    HttpToS3Operator(
-        task_id="fetch",
-        http_conn_id="http_fixturedownload_com",
-        endpoint="/feed/json/epl-2024",
-        s3_bucket="com-cloudership-prod-eu-north-1-airflow-testing",
-        s3_key="2024/football/uk/premier_league-{{ ts }}.json",
-        replace=True,
-    )
+    @task
+    def fetch(logical_date: pendulum.DateTime) -> ObjectStoragePath:
+        endpoint = f"https://fixturedownload.com/feed/json/epl-{YEAR}"
+        path = BUCKET_ROOT / f"{YEAR}/football/uk/premier_league-{logical_date.to_iso8601_string()}.json"
+        with requests.get(endpoint) as response:
+            response.raise_for_status()
+            with path.open("wb") as file:
+                file.write(response.content)
+        return path
+
+    @task
+    def transform(path: ObjectStoragePath):
+        logging.info(path.read_text(encoding="utf-8"))
+
+    transform(fetch())
 
 
 fetch_fixturedownload_football_uk_premier_league()
--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-export _PIP_ADDITIONAL_REQUIREMENTS='apache-airflow[amazon]'
+export _PIP_ADDITIONAL_REQUIREMENTS='aiobotocore boto3 apache-airflow[amazon] apache-airflow-providers-amazon[s3fs] requests'
 
 docker compose up
