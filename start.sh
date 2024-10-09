c05705f722e55bc1b516c3183ccf34dfab83f4ec
Asfand Qazi <ayqazi@gmail.com>
2024-10-09 17:05:07 +0100

    Add apache-airflow[crypto] package

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-export _PIP_ADDITIONAL_REQUIREMENTS='aiobotocore boto3 apache-airflow[amazon] apache-airflow-providers-amazon[s3fs] requests'
+export _PIP_ADDITIONAL_REQUIREMENTS='aiobotocore boto3 apache-airflow[amazon] apache-airflow-providers-amazon[s3fs] apache-airflow[crypto] requests'
 
 docker compose up
