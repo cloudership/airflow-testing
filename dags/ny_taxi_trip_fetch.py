c7131b3b45c2c82050a1a2a9b4ffedb48319632e
Asfand Qazi <ayqazi@gmail.com>
2024-10-22 23:11:38 +0100

    Only fetch, don't parse.

--- dags/ny_taxi_trip_fetch.py
+++ dags/ny_taxi_trip_fetch.py
@@ -4,20 +4,20 @@ from airflow.decorators import dag, task
 from airflow.io.path import ObjectStoragePath
 from pendulum import datetime, DateTime
 
-BUCKET_ROOT = ObjectStoragePath("s3://com-cloudership-prod-eu-north-1-airflow-testing/ny_taxi_trip_prediction/yellow/",
+BUCKET_ROOT = ObjectStoragePath("s3://com-cloudership-prod-eu-north-1-airflow-testing/ny_taxi_trip_prediction/",
                                 conn_id="aws_default")
 
 
-@dag(schedule="@monthly",
+@dag(schedule=None,
      start_date=datetime(2024, 1, 1, tz="UTC"),
-     description="Fetch this month's NY yellow taxi trip data",
+     description="Fetch each month's NY taxi trip data",
      catchup=False)
-def ny_yellow_taxi_trip_prediction_pipeline():
+def ny_yellow_taxi_trip_fetch():
     @task
     def fetch(logical_date: DateTime) -> ObjectStoragePath:
         # Sample: https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet
         endpoint = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{logical_date.year}-{logical_date.month:02}.parquet"
-        path = BUCKET_ROOT / f"{logical_date.year}/{logical_date.month:02}/{logical_date.to_iso8601_string()}.parquet"
+        path = BUCKET_ROOT / f"{logical_date.year}/{logical_date.month:02}/yellow.parquet"
         with requests.get(endpoint) as response:
             response.raise_for_status()
             with path.open("wb") as file:
@@ -25,12 +25,12 @@ def ny_yellow_taxi_trip_prediction_pipeline():
         return path
 
     @task
-    def transform(path: ObjectStoragePath):
+    def check(path: ObjectStoragePath):
         with path.open("rb") as file:
             df = pandas.read_parquet(file)
         print(df.head())
 
-    transform(fetch())
+    check(fetch())
 
 
-ny_yellow_taxi_trip_prediction_pipeline()
+ny_yellow_taxi_trip_fetch()
