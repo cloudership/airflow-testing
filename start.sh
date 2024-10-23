bc006bc422bba22c255a82adebeead32943adfd2
Asfand Qazi <ayqazi@gmail.com>
2024-10-23 16:52:29 +0100

    Make docker compose env more palatable
    * packages in .env, not script
    * no example DAGs

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 
 set -euo pipefail
 
-export _PIP_ADDITIONAL_REQUIREMENTS='aiobotocore boto3 apache-airflow[amazon] apache-airflow-providers-amazon[s3fs] apache-airflow[crypto] requests'
-
 docker compose up
